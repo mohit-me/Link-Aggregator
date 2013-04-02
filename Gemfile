df25dce8ed64a6d278ad87897f78ffe9ca2efb97
mohit-me <mohitmanaktala13@gmail.com>
2013-04-03 03:21:30 +0530

    use unicorn via procfile

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'pg'
 gem 'devise'
 gem 'nokogiri'
 gem 'client_side_validations'
+gem 'unicorn'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,8 @@ LinkTrial1::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
-
+  config.assets.compile = true
+  config.assets.precompile += %w( rails.validations.js )
   # Generate digests for assets URLs
   config.assets.digest = true
 
